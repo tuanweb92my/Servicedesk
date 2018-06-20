a179dc966e76a3b9061fe2b47ba42af2577a95f4
Nguyen Anh Tuan <nguyenanhtuan92my@gmail.com>
2018-06-20 16:14:51 +0700

    change in Helpdesk v2 : lan 2

--- HelpDeskV2.bat
+++ HelpDeskV2.bat
@@ -5,18 +5,18 @@ mkdir c:\Helpdesk
 
 pushd \\vnhocapfs101\OTHERS\RemoteAssistant\
 
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\I_Need_Remote_Assistant_local.bat c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\I_Need_Remote_Assistant_local_outside.bat c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\Search_notassigned_local.bat c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\Search_status_todone_local.bat c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\Search_status_time_local.bat c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\Send_ack_local.bat c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\Send_completed_local.bat c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\Update_inc_local.bat c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\HelpDeskV2_local.bat c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\HelpDesk.ico c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\HelpDesk.lnk c:\Helpdesk
-copy \\vnhocapfs101\OTHERS\RemoteAssistant\JREPL.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\I_Need_Remote_Assistant_local.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\I_Need_Remote_Assistant_local_outside.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\Search_notassigned_local.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\Search_status_todone_local.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\Search_status_time_local.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\Send_ack_local.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\Send_completed_local.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\Update_inc_local.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\HelpDeskV2_local.bat c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\HelpDesk.ico c:\Helpdesk
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\HelpDesk.lnk c:\Helpdeskr
+rem copy \\vnhocapfs101\OTHERS\RemoteAssistant\JREPL.bat c:\Helpdesk
 
 popd
 
