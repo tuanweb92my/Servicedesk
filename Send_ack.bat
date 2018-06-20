69460d4641b5ba79b95eae0ba48805d5ccb577aa
Nguyen Anh Tuan <nguyenanhtuan92my@gmail.com>
2018-06-20 16:40:00 +0700

    thay doi 32bit or 64bit tren Send_ack.bat

--- Send_ack.bat
+++ Send_ack.bat
@@ -2,6 +2,19 @@ set computer1=%1
 
 rem type \\vnhocapfs101\Apps\RemoteAssistant\VNLO_Inc000%2%computer1%.csv >> \\vnhocapfs101\Apps\RemoteAssistant\VNLO_Incident_Report.csv
 
+rem "C:\Program Files (x86)\Microsoft Office\root\Office16\outlook.exe" /c ipm.note /m "DG-vietnam-hoc-IT@webmail.adsint.biz& subject= %username% is working on INC#000%2 on %computer1% &body= Hello all, I ( %username% ) is taking this request. Thanks " 
+
+
+If exist "C:\Program Files (x86)\Microsoft Office\root\Office16\outlook.exe" (
+
 "C:\Program Files (x86)\Microsoft Office\root\Office16\outlook.exe" /c ipm.note /m "DG-vietnam-hoc-IT@webmail.adsint.biz& subject= %username% is working on INC#000%2 on %computer1% &body= Hello all, I ( %username% ) is taking this request. Thanks " 
+)
+
+If exist "C:\Program Files\Microsoft Office\root\Office16\outlook.exe" (
+
+"C:\Program Files\Microsoft Office\root\Office16\outlook.exe" /c ipm.note /m "DG-vietnam-hoc-IT@webmail.adsint.biz& subject= %username% is working on INC#000%2 on %computer1% &body= Hello all, I ( %username% ) is taking this request. Thanks " 
+
+)
+
 
 endlocal
